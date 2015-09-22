e194c9e4b7235ee9672a768224a9378786aa76f3
unknown <GABRIEL.LIMA@DTI-118.sefa.pa.gov.br>
2015-09-22 11:57:08 -0300

    export

--- main.js
+++ main.js
@@ -1,68 +1,68 @@
 var teia = require("teia-search");
 var program = require('commander');
 var fs = require('fs');
-var valido = true;
-
-program
-    .version('1.0.1')
-    .option('-b, --base <n> ', 'Pasta principal com os arquivos que serão filtrados')
-    .option('-r, --restriction <n>', 'Restrições. Ex.: -r branch (apenas filtrará os caminhos que tiverem branch no nome)')
-    .option('-ex, --exclude <n>', 'Exclusões. Ex.: -ex target (irá ignorar os caminhos que tiverem target no nome)')
-    .option('-f, --filter <n>', 'Arquivo com os dados que serão filtrados')
-    .option('-o, --result <n>', 'Arquivo com os resultados da filtragem')
-    .option('-i, --index <i>', 'Indice split do caminho que contem o nome do projeto')
-    .option('-d, --debug', 'Modo debug')
-    .option('--show', 'Abre o Arquivo ao final do processamento')
-    .parse(process.argv);
-/**/
-if (!process.argv.slice(2).length) {
-    program.outputHelp();
-}
-/**/
-var options = {};
-var errs = "";
-/**/
-if (program.base !== undefined) {
-    options.base = program.base;
-}
-if (program.restriction !== undefined) {
-    options.restriction = program.restriction;
-}
-if (program.exclude !== undefined) {
-    options.exclude = program.exclude;
-}
-if (program.filter !== undefined) {
-    if (!fs.existsSync(program.filter)) {
-        addErro("Insira um caminho valido para o filtro!");
+module.exports = function() {
+    program
+        .version('1.0.1')
+        .option('-b, --base <n> ', 'Pasta principal com os arquivos que serão filtrados')
+        .option('-r, --restriction <n>', 'Restrições. Ex.: -r branch (apenas filtrará os caminhos que tiverem branch no nome)')
+        .option('-ex, --exclude <n>', 'Exclusões. Ex.: -ex target (irá ignorar os caminhos que tiverem target no nome)')
+        .option('-f, --filter <n>', 'Arquivo com os dados que serão filtrados')
+        .option('-o, --result <n>', 'Arquivo com os resultados da filtragem')
+        .option('-i, --index <i>', 'Indice split do caminho que contem o nome do projeto')
+        .option('-d, --debug', 'Modo debug')
+        .option('--show', 'Abre o Arquivo ao final do processamento')
+        .parse(process.argv);
+    /**/
+    if (!process.argv.slice(2).length) {
+        program.outputHelp();
+    }
+    /**/
+    var options = {};
+    var errs = "";
+    /**/
+    if (program.base !== undefined) {
+        options.base = program.base;
+    }
+    if (program.restriction !== undefined) {
+        options.restriction = program.restriction;
+    }
+    if (program.exclude !== undefined) {
+        options.exclude = program.exclude;
+    }
+    if (program.filter !== undefined) {
+        if (!fs.existsSync(program.filter)) {
+            addErro("Insira um caminho valido para o filtro!");
+        } else {
+            options.filefilter = program.filter;
+        }
+    }
+    /**/
+    function addErro(erro) {
+        if (errs.length === 0) {
+            errs += "\n =========================================\n";
+            errs += "\n \tOcorreram os seguintes erros\n\n";
+        }
+        errs += "\t * " + erro + "\n";
+    }
+    if (errs.length > 0) {
+        errs += "\n\n =========================================\n\n";
+        console.log(errs);
+    }
+    if (program.result !== undefined) {
+        options.fileResult = program.result;
+    }
+    if (program.index !== undefined) {
+        options.indexpath = program.index;
+    }
+    if (options.base !== undefined && options.filefilter !== undefined && options.fileResult !== undefined) {
+        teia.init(options);
+        teia.debug(program.debug);
+        teia.run();
+        if (program.show) {
+            require('child_process').exec(options.fileResult);
+        }
     } else {
-        options.filefilter = program.filter;
+        program.outputHelp();
     }
-}
-/**/
-function addErro(erro) {
-    if (errs.length === 0) {
-        errs += "\n =========================================\n";
-        errs += "\n \tOcorreram os seguintes erros\n\n";
-    }
-    errs += "\t * " + erro + "\n";
-}
-if (errs.length > 0) {
-    errs += "\n\n =========================================\n\n";
-    console.log(errs);
-}
-if (program.result !== undefined) {
-    options.fileResult = program.result;
-}
-if (program.index !== undefined) {
-    options.indexpath = program.index;
-}
-if (options.base !== undefined && options.filefilter !== undefined && options.fileResult !== undefined) {
-    teia.init(options);
-    teia.debug(program.debug);
-    teia.run();
-    if (program.show) {
-        require('child_process').exec(options.fileResult);
-    }
-} else {
-    program.outputHelp();
 }
